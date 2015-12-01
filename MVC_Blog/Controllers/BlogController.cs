ff35bead2eeb5f34b69999116c96bc92612d16de
Carsten Nielsen <carsten.beck.nielsen@hotmail.com>
2015-12-01 10:47:57 +0100

    En helt masse nyt

---
+++ MVC_Blog/Controllers/BlogController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using REPOBlog;
+
+namespace MVC_Blog.Controllers
+{
+    public class BlogController : Controller
+    {
+        PostFac postFac = new PostFac();
+        // GET: Blog
+        public ActionResult Index()
+        {
+            
+            return View(postFac.GetIndexData());
+        }
+    }
+}
--- REPOBlog/Factories/PostFac.cs
+++ REPOBlog/Factories/PostFac.cs
@@ -4,10 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using REPOBlog.Models.BaseModels;
+using REPOBlog.Models.ViewModels;
 
 namespace REPOBlog
 {
     public class PostFac : AutoFac<Post>
     {
+        BilledeFac billedeFac = new BilledeFac();
+
+        public List<BlogIndex> GetIndexData()
+        {
+            List<BlogIndex> listBlogindex = new List<BlogIndex>();
+            foreach (var post in GetAll())
+            {
+                BlogIndex blogIndex = new BlogIndex();
+                blogIndex.Post = post;
+                blogIndex.Billede = billedeFac.GetBy("PostID", post.ID);
+                listBlogindex.Add(blogIndex);
+            }
+            return listBlogindex;
+        } 
     }
 }
